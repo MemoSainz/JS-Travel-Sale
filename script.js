7d484b57ba55dfd607cf5d88108b0f24b851d00b
MemoSainz <sainzmemo_21@proton.me>
2024-02-16 01:54:16 -0700

    Travel Sales in JavaScript

---
+++ script.js
@@ -0,0 +1,45 @@
+import { barcelona, rome, paris, london } from './ciudades.js'
+                                               
+// Obtener los elementos del DOM
+
+let enlaces = document.querySelectorAll("a")
+let tituloElemento = document.getElementById('titulo')
+let subTituloElemento = document.getElementById('subtitulo')
+let parrafoElemento = document.getElementById('parrafo')
+let precioElemento = document.getElementById('precio')
+
+console.log(enlaces)
+
+//Agregar un evento CLICK a cada enlace
+enlaces.forEach(function (enlace) {
+    enlace.addEventListener('click', function () {
+        // Remover la clase "active" de todos los enlaces
+        enlaces.forEach(function (enlace) {
+            enlace.classList.remove('active');
+        });
+
+
+    // Agregar la clase "active" al enlace actual
+    this.classList.add('active')
+
+    // Obtener el contenido correspondiente según el enlace
+    let contenido = obtenerContenido(this.textContent)
+
+    tituloElemento.innerHTML = contenido.titulo
+    subTituloElemento.innerHTML = contenido.subtitulo
+    parrafoElemento.innerHTML = contenido.parrafo
+    precioElemento.innerHTML = contenido.precio
+    });
+
+});
+
+// Función para traer la información correcta desde ciudades.js
+function obtenerContenido(enlace) {
+    let contenido = {   
+        'Barcelona' : barcelona,
+        'Rome' : rome,
+        'Paris' : paris,
+        'London' : london
+    }
+    return contenido[enlace];
+}
